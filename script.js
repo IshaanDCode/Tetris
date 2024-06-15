99ec7536a7c49a6e0d6f59b44206ed71f2749e48
Ishaan Dhiman <ishaandhiman74@gmail.com>
2024-06-15 16:14:40 -0400

    Made some changes

---
+++ script.js
@@ -0,0 +1,18 @@
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = math.floor(max);
+
+    return Math.floor(math.random() * (max - min + 1)) + min;
+
+}
+
+function generateSequence(){
+    const sequence = ['I', 'J', 'O', 'S', 'T', 'Z'];
+
+    while(sequence.lenght){
+        const rand = getRandomInt(0, sequence.lenght - 1);
+        const name = sequence.splice(rand,1)[0];
+        tetrominoSequence.push(name);
+        
+    }
+}
